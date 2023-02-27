c6835437dc078e31bd503b63ab49f6bf347ee111
EMRE AYTAS <emreayta01@gmail.com>
2023-02-27 03:15:39 +0300

    degisimlervar

---
+++ first.py
@@ -0,0 +1,13 @@
+website = "https://www.sadikturan.com"
+website1 = website
+course = "Python kursu: Bastan sona python programlama rehberiniz (40 saat)"
+print("course karakter dizisi uzunlugu: "+str(len(course)))
+
+
+
+print(website1)
+print(course[:15]+"..."+course[len(course) - 16 : len(course) - 1])        
+print(course[len(course) - 1::-1])
+
+name,surname,age,job = "emre","aytas",20,"job"
+print(f"Benim adim {name} {surname}, yasim {age} meslegim {job}")
