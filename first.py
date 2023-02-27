cf5dbbf32ed5e310325136e3460442b48357b9ce
EMRE AYTAS <emreayta01@gmail.com>
2023-02-27 15:28:25 +0300

    git ogreniyorum

--- first.py
+++ first.py
@@ -9,4 +9,4 @@ print(course[:15]+"..."+course[len(course) - 16 : len(course) - 1])
 print(course[len(course) - 1::-1])
 
 name,surname,age,job = "emre","aytas",20,"job"
-print(f"Benim adim {name} {surname}, yasim {age} meslegim:: {job}")
+print(f"Benim adim {name} {surname}, yasim {age} meslegim: {job}")
