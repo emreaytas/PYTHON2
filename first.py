9f3104eac50f4e49a6f90604b2a5097d1d6b95d8
EMRE AYTAS <emreayta01@gmail.com>
2023-03-05 17:57:05 +0300

    new123

--- first.py
+++ first.py
@@ -1,2 +1,33 @@
 names = ["emre","arif","ford","aysa"]
 surnames = ["aytas","aytas","focus","liya"]
+
+
+
+dict1 = {"emre":"aytas","ford":"focus",6:"dddd",56:24.4,"emre":"dddd"}
+
+
+users ={
+    
+    "emre":{
+       "isim":"emre",
+       "soyisim":"aytas",
+       "yas":20
+    }
+,
+    "ford":{
+       "isim":"ford",
+       "soyisim":"focus",
+       "yas":7
+    }
+
+}
+
+print(users["emre"]["isim"])
+print(users["emre"]["yas"])
+
+users["emre"] = {       "isim":"emre",
+       "soyisim":"aytas",
+       "yas":31}
+
+for i in users:
+    print(i)
