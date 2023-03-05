2a969448dc057811ca1462fece8d2861e4ccf0f9
EMRE AYTAS <emreayta01@gmail.com>
2023-03-05 23:57:31 +0300

    new990

--- first.py
+++ first.py
@@ -1,38 +1,10 @@
-ogrenciAdeti = int(input("Kac adet ogrenci gireceksiniz: "))
+set1 = {"apple","orange","ford","focus","mercedes","bmw"}
+set1.remove("apple")
+#set1.remove("mclaren")
+set1.discard("ford")
+set1.discard("alfa")
 
-students = {}
-i = 0
-for i in range(ogrenciAdeti):
-    numara = int(input(str(i + 1)+". ogrencinin numarasini giriniz: "))
-    if numara in students:
-        print("Bu kisi bulunuyor...")
-        i = i-1
-        continue
-    
-    elif numara < 100 or numara > 999:
-        print("Tanimlanamaz numara...")
-        i = i-1
-        continue
-    else:
-        name = input("Ogrencinin ismini giriniz: ")
-        surname = input("Ogrencinin soyadini giriniz: ")
-        phone = None
-        phone = input("Ogrencinin numarasini giriniz: ")
-        while not len(phone) == 11:
-            
-            phone = input("Ogrencinin numarasini giriniz: ")
-            
-        students[str(numara)] = {
-            'ad':name,
-            'soyad':surname,
-            'phone':phone
-        }
-        
-for i in students:
-    print(str(i) +": "+students[i]["ad"]+" "+students[i]["soyad"]+" "+students[i]["phone"])
-         
-        
-    
-    
+
+
     
     
