8895ff3872934c40adfc16f1da35b37d43b0d7f8
EMRE AYTAS <emreayta01@gmail.com>
2023-03-06 23:25:10 +0300

    trtrt

--- first.py
+++ first.py
@@ -1,8 +1,8 @@
-set1 = {"apple","orange","ford","focus","mercedes","bmw"}
-set1.remove("apple")
-#set1.remove("mclaren")
-set1.discard("ford")
-set1.discard("alfa")
+x = 5
+y = 10
+x,y = y,x
+print(str(x)+" "+str(y))
+
 
 
 
