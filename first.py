ec1f1e92c7bcd585c05ea5ab2466c9a4b0302892
EMRE AYTAS <emreayta01@gmail.com>
2023-04-04 23:53:03 +0300

    newtry

--- first.py
+++ first.py
@@ -1,8 +1,34 @@
+class User():
+    pass
+    
+class UserRepository():
+    
+    def register(self):
+        pass
+    
+    def login(self):
+        pass
+    
+    def saveToFile(self):
+        pass 
 
-import datetime
 
-import os
 
-os.mkdir("honda.txt")
-os.rename("honda.txt","fordfocus.txt")
-os.rmdir("fordfocus.txt")
+while True:
+    print("MENU".center(50,'*'))
+    print("")
+    secim = input("1-Register \n2-Login\n3-Logout\n4-Identy\n5-Exit")
+    if secim == '5':
+        break
+    elif secim == '4':
+        pass
+    elif secim == '3':
+        pass
+    elif secim == '2':
+        pass
+    elif secim == '1':
+        pass
+    else:
+        print("Wrong choose...\n")d
+    
+    
