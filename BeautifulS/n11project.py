5acadc4b79db5b7648613881ce5852f966916f05
EMRE AYTAS <emreayta01@gmail.com>
2023-04-12 18:12:49 +0300

    ----twitter botu okeyyy

--- BeautifulS/n11project.py
+++ BeautifulS/n11project.py
@@ -9,12 +9,15 @@ html = requests.get(site_url).content
 soup = bs(html,'html.parser') # parse etmek istediğimiz içerik ve onun parser tipini belirledik...
 
 liste = soup.find_all("li",{"class":"column"}) # classı column olan li'leri aldık...
+
+intw = 1 
  
 for li in liste:
     name = li.div.a.h3.text.strip() # sırası ile tek tek içeri girerek bilgiyi çektik... strip ile bolşukları sildik...
     link = li.div.a.get("href") # a etiketi içerisinden get ile href yani link aldık...
-    fiyat = li.find("div",{"class":"proDetail"}).find_all("a")[0] 
-    newfiyat = li.find("div",{"class":"proDetail"}).find_all("a")[1]
+    fiyat = li.find("div",{"class":"proDetail"}).find_all("a")[0].text.strip('TL')
+    newfiyat = li.find("div",{"class":"proDetail"}).find_all("a")[1].text.strip('TL')
      
-    print(f"{fiyat} -> {newfiyat}")
+    print(f"{intw}-isim: {name} link: {link} eksi fiyat: {fiyat} -> yeni fiyat: {newfiyat}")
+    intw += 1
 
---
+++ Selenium/tweetat.py
@@ -0,0 +1,79 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+import time
+import json
+from selenium.webdriver.common.keys import Keys
+from datetime import datetime
+
+# ms Edge üzeriden calisacak... Edge sürümü = 112.0.1722.34  ....... x86 dosyası inerek bu .py dosaysının pathi üzerine getirilmelidir...
+
+email = "" # kullanici adi girilecek veya e posta str olarak...
+password = "" # kullanci sifresi girilecek str olarak...
+tweet = input("Atacaginiz tweeti giriniz: ")
+
+class Twitter():
+    def __init__(self,email,password,tweet):
+        self.devam = True
+        self.driver = webdriver.Edge()
+        self.email = email
+        self.password = password
+        self.zaman = datetime.now()
+        self.tweet = f"- Tweet atilma zamani: {self.zaman.year}/{self.zaman.month}/{self.zaman.day} {self.zaman.hour}:{self.zaman.minute}:{self.zaman.second} --- {tweet}"
+    
+    def SignIn(self):
+        self.driver.get("https://twitter.com/i/flow/login")
+        time.sleep(10)
+        self.driver.maximize_window()
+        
+        emailinput = self.driver.find_element(By.XPATH,"//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div/div/div/div[5]/label/div/div[2]/div/input")               
+        time.sleep(3)
+        emailinput.send_keys(self.email,Keys.ENTER)
+        time.sleep(3)
+       
+        time.sleep(3)
+        passwordinput = self.driver.find_element(By.XPATH,"//*[@id='layers']/div/div/div/div/div/div/div[2]/div[2]/div/div/div[2]/div[2]/div[1]/div/div/div[3]/div/label/div/div[2]/div[1]/input")
+        passwordinput.send_keys(self.password,Keys.ENTER)
+        time.sleep(10)
+        
+    def tweetAt(self):
+        time.sleep(5)
+        self.driver.find_element(By.XPATH,"//*[@id='react-root']/div/div/div[2]/header/div/div/div/div[1]/div[3]/a").click()
+        time.sleep(5)
+        tweetinput = self.driver.find_element(By.XPATH,"//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div")
+        time.sleep(3)
+        tweetinput.send_keys(self.tweet)
+        time.sleep(3)
+        self.driver.find_element(By.XPATH,"//*[@id='layers']/div[2]/div/div/div/div/div/div[2]/div[2]/div/div/div/div[3]/div[2]/div[1]/div/div/div/div/div[2]/div[3]/div/div/div[2]/div[4]/div/span/span").click()
+        time.sleep(3)
+        
+        newTweet = input("Atacaginiz yeni tweeti giriniz(cikmak isterseniz q harfini giriniz): ")
+        self.tweet = newTweet
+        if self.tweet == "q":
+            self.devam = False
+            return
+        self.zaman = datetime.now()
+        self.tweet = f"- Tweet atilma zamani: {self.zaman.year}/{self.zaman.month}/{self.zaman.day} {self.zaman.hour}:{self.zaman.minute}:{self.zaman.second} --- {self.tweet}"
+                   
+        """
+        //*[@id="react-root"]/div/div/div[2]/main/div/div/div/div/div/div[3]/div/div[2]/div[1]/div/div/div/div[2]/div[1]/div/div/div/div/div/div[2]/div/div/div/div/label/div[1]/div/div/div/div/div/div[2]/div/div/div/div
+        """        
+        
+        
+twitter = Twitter(email,password,tweet)
+       
+twitter.SignIn()
+
+while True:
+
+    twitter.tweetAt()        
+    if twitter.devam == False:
+        print("Cikis yapiliyor...")
+        time.sleep(1.5)
+        break        
+        
+        
+twitter.driver.close()
+
+
+
