07e149ea5095d7979a3dff9d884aa91a228b5eeb
EMRE AYTAS <emreayta01@gmail.com>
2023-05-01 23:56:21 +0300

    asdvbdafb

--- PANDAS/pandaspy.py
+++ PANDAS/pandaspy.py
@@ -6,8 +6,6 @@ df = pd.read_csv("PANDAS/sample1.csv") # csv okumak için read_csv kullanırız.
 df1 = pd.read_json("PANDAS/sample.json") # json okumak için ise read_json kullanırız... eğer ezcelden okuma yapacaksak o zaman pd.read_excel kullanırız ama ayrı bir kütüphane lazım pip install xlrd ile bunu indirmemiz lazım eğer excelden veri okumak istersek...
 # excelden veri okumak için ise read_excel kullanırız xlsx ise excel uzantısıdır...
 
-
-
 print(df)
 print(df1)
 
