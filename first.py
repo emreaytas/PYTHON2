87ac72fc9f6c0d8c448c3fd2ce9513830d503c9d
EMRE AYTAS <emreayta01@gmail.com>
2023-05-19 16:48:28 +0300

    fdghdfgb

--- first.py
+++ first.py
@@ -1,12 +1,17 @@
 import sqlite3 as sql 
 
-conn1 = sql.connect("ders.db")
-cursor1 = conn1.cursor()
+conn = sql.connect("ders.db")
+cursor = conn.cursor()
 
-cursor1.execute()
+cursor.execute("""select * from calisanlar where ad in('ahmet','emre','ford','bmw')""")
+listall = cursor.fetchall()
 
-conn1.commit()
-conn1.close()
+for calisan in listall:
+    print(calisan)
+    
+    
+conn.commit()
+conn.close()
 
 
 """
@@ -144,8 +149,63 @@ def deleteAge(age):
 
 
 '''
+
 tablodaki verileri yazdırma...
 
+import sqlite3 as sql 
+
+conn1 = sql.connect("ders.db")
+cursor1 = conn1.cursor()
+
+cursor1.execute("""select * from students""")
+tumdegerler = cursor1.fetchall() # fetchall() ile imlecin getirdiği değerleri atarız. bir liste içerisinde getirir bize. imlecin seçtiği elemanları almak için kullanırız.
+                      #.fetchone olarak kullanırsak eğer ilk elemanı alır bir daha kullanırsak ikinci satırı alır tıpkı dosyalardaki imleç gibi çalışır.
+                         
+                      # mesela iki kere cursor1.fecthone() kullandıktan sonra cursor1.fetchall() kullanırsak eğer ilk iki değer hariç tüm hepsini alır değer derken satır yani çünkü verileri satır satır tutar sql... 
+                      
+                      
+                         
+print(type(tumdegerler)) # <class 'list'> 
+
+conn1.commit()
+conn1.close()
+
+select * from students where not age >= 40 # böylece yaşı 40tan küçük olanları alırız not ile koşulu tersine çeviririz.
+
+....................................................................................
+
+# fetchamny = cursor1.fetchmany(3) # ilk üç satırı bize getirir ilk üç veriyi verir bize. ama eğer içeri sıfır verirsek tüm değerleri getirir. eğer 100 yazarsak ve elde o kadar veri yoksa ne kadar varsa hepsini getirir hata çıkarmadan.
+
+# cursor.execute("""select rowid,* from students""") # rowid dahil hepsini getirir. ama burada dikkat etmemiz gereken bir şey var rowid ve * ile gleen değerler tek bit tuplle içerisinde gelecek. yani her veriyi elde edileni bir tupple içerisinde bize geri sunacak.
 
 
 '''
+
+'''
+def printAll():
+    conn1 = sql.connect("ders.db")
+    cursor1 = conn1.cursor()
+
+    cursor1.execute("""select * from students""")
+    tumdegerler = cursor1.fetchall()
+    for satir in tumdegerler:
+        print(satir)
+
+    conn1.commit()
+    conn1.close()
+''' # tüm verileri yazdıran bir fonksiyon.
+
+
+'''
+
+cursor.execute("""select * from calisanlar where ad in('ahmet','emre','ford','bmw')""") # bu yapı ile birden fazla and koymaya gerek kalmaz bir tuple içerisindeki verilerden getirir. in bu işe yarar içeride olanlara göre verileri getirir. where ad not in('emre','bmw','ford') böyle olursa eğer o zaman bu içeride olmayanları getirecek.
+
+cursor.execute("""select * from calisanlar where maas between 1500 and 3000""") # böylece maasları 1500 ile 3000 olanları bize getirir koşul ifadesi yazmaya gerek kalmaz 1500 ve 3000i dahil etti..
+
+cursor.execute("""select * from calisanlar where ad like 'ah%'""") # ismin baş kısmı ah olanları getirir... '%ah'  olursa sonu ah ile bitenleri arar...  ortada olanları ise '%ah%' ortasında bir yerde ah olanları getirir.
+                                                                   # büyük küçük harf ayrımı yapmaz. 
+                                                                    
+cursor.execute("""select * from calisanlar where ad like 'm___e_' """) # ilk harfi m 4. harfi e olan isme göre değerleri bize getirir.
+
+'''
+
