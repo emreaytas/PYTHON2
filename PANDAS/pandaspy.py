f2530694dbb0179ffdff26731eb7af14bb5e279f
EMRE AYTAS <emreayta01@gmail.com>
2023-05-23 00:01:30 +0300

    zsfvzbvfxb

--- PANDAS/pandaspy.py
+++ PANDAS/pandaspy.py
@@ -1,12 +1,4 @@
 import pandas as pd
 import numpy as np
 
-# path önemli çünkü okuma işlemine PYTHONDAN başladı bu yüzden PANDAS klasörüne girmek gerekti...
-df = pd.read_csv("PANDAS/sample1.csv") # csv okumak için read_csv kullanırız...         
-df1 = pd.read_json("PANDAS/sample.json") # json okumak için ise read_json kullanırız... eğer ezcelden okuma yapacaksak o zaman pd.read_excel kullanırız ama ayrı bir kütüphane lazım pip install xlrd ile bunu indirmemiz lazım eğer excelden veri okumak istersek...
-# excelden veri okumak için ise read_excel kullanırız xlsx ise excel uzantısıdır...
-
-print(df)
-print(df1)
-
-
+print(pd.__version__) # 2.0.0 bu yapı ile pandasın verisyonunu görürüz. pip install --user --upgrade pandas ile ise güncelleme yapabiliriz.
--- second.py
+++ second.py
@@ -1,30 +1,45 @@
-class Person():
-    def __init__(self,name,surname):
-        self.name = name
-        self.surname
-    
-    
-def listeTekle(list1):
-    liste2 = []
-    for i in list1:
-        if i in liste2:
-            continue
-        else:
-            liste2.append(i)
-    return liste2
-          
-"""
-    import second
-    liste = ["3","33","2","fdfddf",2343,21423,"edffff",342]
-    list1 = second.sadeceSayilar(liste)
-    print(list1) #['3', '33', '2', 2343, 21423, 342]
-"""
-def sadeceSayilar(x):
-    y = []
-    for i in x:
-        try:
-            value = int(i)
-            y.append(i)
-        except:
-            continue    
-    return y
+import random
+liste1 = ["emre","ceren","evren","fati","emir","zehra"] 
+
+deger = liste1[random.randint(0,5)]
+
+print(deger)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
