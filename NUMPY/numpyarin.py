28f5f28ade31fa6ff6af1a49f0fcd404a0fe4bb3
EMRE AYTAS <emreayta01@gmail.com>
2023-06-25 16:03:42 +0300

    fgdfgndfngf

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,5 +1,8 @@
 import numpy as np
 
-x = np.array([1.5,2.5,3.6,4.3],dtype=np.int32) # veriler int olarak tutulacak. yani noktadan sonrası gidecek upcasting değil downcasting olacak. float veriden int dönüşümü yaptırdık bunu dtype ile yaptık.  
-print(x) #[1 2 3 4]
-print(x.dtype) # int 3
+x = np.array([1,2,3,4,5],dtype=np.int64)
+np.save("fordfocus",x) # np.save ile array kaydederiz. önce kaydedilecek yer sonra ise kaydedilecek değeri gireriz ve .npy uzantılı bir dosyaya değerleri kaydeder.
+
+y = np.load("fordfocus.npy") # y = dosya içerisindeki ndarray olacak aslında. np.load() parametre alacak oda arrayin kaydedildiği .npy dosyası.
+
+print(y) # [1 2 3 4 5]
