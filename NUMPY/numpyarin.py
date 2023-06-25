b82a690aa97fd399a9ecd979b03b654ddffc4f4c
EMRE AYTAS <emreayta01@gmail.com>
2023-06-25 04:43:12 +0300

    zdsfngmnagma f

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,5 +1,5 @@
 import numpy as np
 
-x = np.array([1,2,3.0,'4'])
-print(x.dtype) #<U32 karakter setini verdi hepsini string galine çevirdi yani.
-
+x = np.array([1.5,2.5,3.6,4.3],dtype=np.int32) # veriler int olarak tutulacak. yani noktadan sonrası gidecek upcasting değil downcasting olacak. float veriden int dönüşümü yaptırdık bunu dtype ile yaptık.  
+print(x) #[1 2 3 4]
+print(x.dtype) # int 3
