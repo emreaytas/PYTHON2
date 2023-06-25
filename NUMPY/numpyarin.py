158b869303edc8a965ad8633649bc56038794e21
EMRE AYTAS <emreayta01@gmail.com>
2023-06-25 18:43:48 +0300

    fdfgbndfng

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,8 +1,6 @@
 import numpy as np
 
-x = np.array([1,2,3,4,5],dtype=np.int64)
-np.save("fordfocus",x) # np.save ile array kaydederiz. önce kaydedilecek yer sonra ise kaydedilecek değeri gireriz ve .npy uzantılı bir dosyaya değerleri kaydeder.
+x = np.array([1,2,3,4,5,6,7])
+x.fill(100)
 
-y = np.load("fordfocus.npy") # y = dosya içerisindeki ndarray olacak aslında. np.load() parametre alacak oda arrayin kaydedildiği .npy dosyası.
-
-print(y) # [1 2 3 4 5]
+print(x)
