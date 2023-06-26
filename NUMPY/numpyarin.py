c2e30556c8ac8f0251978ff23254d8b1c5a8a255
EMRE AYTAS <emreayta01@gmail.com>
2023-06-26 18:26:03 +0300

    sfmngsfgmsgmnsfn bfgnd

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,11 +1,4 @@
 import numpy as np
 
-x = np.identity(5)
-print(x)
-"""
-[[1. 0. 0. 0. 0.]
- [0. 1. 0. 0. 0.]
- [0. 0. 1. 0. 0.]
- [0. 0. 0. 1. 0.]
- [0. 0. 0. 0. 1.]]
-""" # 5 e 5 birim matris oluşturur...
+
+
