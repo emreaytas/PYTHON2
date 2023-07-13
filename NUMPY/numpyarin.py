54128c4d09ebc5fa80379fe5be2d5e559e0024ad
EMRE AYTAS <emreayta01@gmail.com>
2023-07-13 17:44:33 +0300

    rjrejtert

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,11 +1,4 @@
 import numpy as np
 
-x = np.eye(4,5)
-print(x)
-"""
-[[1. 0. 0. 0. 0.]
- [0. 1. 0. 0. 0.]
- [0. 0. 1. 0. 0.]
- [0. 0. 0. 1. 0.]]
-
-"""
+x = np.arange(20)
+x = np.reshape(x,(4,5)) # önce hangi array shape olacak sonra ise tuple içerisinde hangi yapıda olacak ve bu elde edilen yaoıyı return eder.
---
+++ PANDAS/.ipynb_checkpoints/pandaspy-checkpoint.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+
+print(pd.__version__) # 2.0.0 bu yapı ile pandasın verisyonunu görürüz. pip install --user --upgrade pandas ile ise güncelleme yapabiliriz.
+
+
+
+
+
+
