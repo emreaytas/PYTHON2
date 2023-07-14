eca9eba4dc9a4df8d98b2a9dd92eecdea1df2e95
EMRE AYTAS <emreayta01@gmail.com>
2023-07-14 17:01:35 +0300

    dsfbsdfbs

--- NUMPY/numpyarin.py
+++ NUMPY/numpyarin.py
@@ -1,4 +1,12 @@
 import numpy as np
 
-x = np.arange(20)
-x = np.reshape(x,(4,5)) # önce hangi array shape olacak sonra ise tuple içerisinde hangi yapıda olacak ve bu elde edilen yaoıyı return eder.
+x = np.arange(9).reshape(3,3)
+x = np.insert(x,1,[100,100,100],axis=0) # axis 0 olduğu için satıra ekleme olacak. ilk olarak işlem görecek olanı gireriz sonra index sonra ne eklenecek sonra axis ve np.insert() bize işlemden çıkmış arrayin referansını return eder. atama olduğu için referans kalır bellekten silinmez.
+print(x)
+"""
+[[  0   1   2]
+ [100 100 100]
+ [  3   4   5]
+ [  6   7   8]]
+ 
+"""
