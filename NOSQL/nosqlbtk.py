aa43da10e96d6613db428fb5128d972378d28657
EMRE AYTAS <emreayta01@gmail.com>
2023-07-30 22:59:25 +0300

    dataframe

--- NOSQL/nosqlbtk.py
+++ NOSQL/nosqlbtk.py
@@ -1,18 +1,13 @@
 
-url = "mongodb+srv://emreaytas:"+"34ffm72"+"@cluster0.mq5dsce.mongodb.net/?retryWrites=true&w=majority"
+url = "mongodb://localhost:27017/"
 
 from pymongo.mongo_client import MongoClient
 myclient = MongoClient(url)
 try:
-    myclient.admin.command('ping')
-    print("Pinged your deployment. You successfully connected to MongoDB!")
+    mydb = myclient["node_app"] # önceden var olan bir db olmalı.
+    mycollection = mydb["collection1"] # önceden var edilmiş bir collection olmalı.
+    print(myclient.list_database_names())
+    
 except Exception as e:
     print(e)
 
-
-mydb = myclient["node_app"] # önceden var olan bir db olmalı.
-mycollection = mydb["collection1"] # önceden var edilmiş bir collection olmalı.
-
-data = {"name":"emre","surname":"aytas"}
-x = mycollection.insert_one(data)
-print(x)
