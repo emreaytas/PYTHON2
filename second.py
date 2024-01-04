f9dfdee6d921c034cd20d8de4bcadd25242f34b3
EMRE AYTAS <emreayta01@gmail.com>
2024-01-05 00:29:14 +0300

    oopbtk

--- second.py
+++ second.py
@@ -1,11 +1,18 @@
-import random
-liste1 = ["emre","ceren","evren","fati","emir","zehra"] 
+import datetime 
+dolarKuru = 30.72
+if datetime.date(2024,7,1) == datetime.datetime.now().date:
+    if dolarKuru >= 37:
+        print("SORUN BÜYÜK...")
+    else:
+        print("DEMEK Kİ BİR ŞEYLER YOLUNDA...")
+else:
+    if dolarKuru >= 37:
+        print("SORUN ÇOKTAN GELMİŞ...")
+    else:    
+        print("DAHA O ZAMANLAR GELMEDİ...")
+    
 
-deger = liste1[random.randint(0,5)]
-
-print(deger)
-
-
+            
 
 
 
